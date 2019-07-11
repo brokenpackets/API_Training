c795a0b1f05008a25fdc9affef50bc9e6055dcdb
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2019-07-11 12:16:39 -0700

    Add example using built-in requests library.
    Useful if jsonrpclib not available.

---
+++ Requests-example.py
@@ -0,0 +1,42 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+devicelist = ['192.168.255.254','192.168.255.6']
+currentuser = 'admin'
+currentpass = 'Arista'
+get_hostname = ['enable', 'show hostname']
+get_version = ['enable', 'show version']
+outputlist = []
+
+def eapi(switch,cmds):
+    jsondata = {
+      "jsonrpc": "2.0",
+      "method": "runCmds",
+      "params": {
+        "format": "json",
+        "timestamps": False,
+        "autoComplete": False,
+        "expandAliases": False,
+        "cmds": cmds,
+        "version": 1
+      },
+      "id": "EapiExplorer-1"
+    }
+    r = requests.post('https://' + switch + '/command-api', data=json.dumps(jsondata), verify=False,
+                  auth=HTTPBasicAuth(currentuser, currentpass))
+    return json.loads(r.text)
+
+def main():
+    for switch in devicelist:
+        hostname = eapi(switch,get_hostname)['result'][1]['hostname']
+        version = eapi(switch,get_version)['result'][1]['version']
+        outputlist.append(hostname+','+version)
+    print '-------Devices--------'
+    for item in outputlist:
+        print item
+
+if __name__ == "__main__":
+    main()
