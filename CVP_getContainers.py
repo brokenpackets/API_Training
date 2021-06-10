3f7d01d1e2a4214cd2c66f916e0f10111cc45f5c
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2021-06-10 09:17:12 -0700

    Create CVP_getContainers.py

---
+++ CVP_getContainers.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import requests
+import json
+
+###### User Variables
+
+username = 'admin'
+password = 'Arista123'
+server1 = 'https://192.168.255.50'
+upload = False
+
+###### Rest of script.
+connect_timeout = 10
+headers = {"Accept": "application/json",
+           "Content-Type": "application/json"}
+session = requests.Session()
+
+def login(url_prefix, username, password):
+    authdata = {"userId": username, "password": password}
+    headers.pop('APP_SESSION_ID', None)
+    response = session.post(url_prefix+'/web/login/authenticate.do', data=json.dumps(authdata),
+                            headers=headers, timeout=connect_timeout,
+                            verify=False)
+    cookies = response.cookies
+    headers['APP_SESSION_ID'] = response.json()['sessionId']
+    if response.json()['sessionId']:
+        return response.json()['sessionId']
+
+def logout(url_prefix):
+    response = session.post(url_prefix+'/web/login/logout.do')
+    return response.json()
+
+def get_containers(url_prefix):
+    response = session.get(url_prefix+'/cvpservice/inventory/containers')
+    return response.json()
+
+def main():
+    print '###### Logging into Server 1'
+    login(server1, username, password)
+    containerList = get_containers(server1)
+    for container in containerList:
+        print 'Container: '+container['Name']
+    logout(server1)
+
+if __name__ == "__main__":
+  main()
