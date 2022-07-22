4577ade347af988e7ddf9d101a188ea33ffc136f
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2022-07-22 09:52:14 -0700

    Update interfaceMapper.py

--- interfaceMapper.py
+++ interfaceMapper.py
@@ -2,7 +2,6 @@
 from jsonrpclib import Server
 import json
 import ssl
-from pprint import pprint
 from operator import itemgetter
 import re
 
@@ -12,26 +11,31 @@ user = "admin"
 passwd = 'Arista123'
 ssl._create_default_https_context = ssl._create_unverified_context
 
-switch = '192.168.255.254'
+switches = ['192.168.255.254']
 
 def main():
     #SESSION SETUP FOR eAPI TO DEVICE
-    url = "https://%s:%s@%s/command-api" % (user, passwd, switch)
-    ss = Server(url)
-    #CONNECT TO DEVICE
-    hostname = ss.runCmds( 1, ['enable', 'show hostname' ])[1]['hostname']
-    interfaces = ss.runCmds( 1, ['enable', 'show interfaces hardware default' ])[1]['interfaces']
-    speedList = []
-    print interfaces
-    for interface in interfaces:
-        print '\n'
-        #print interfaces[interface]
-        positional = interface.replace('Ethernet','')
-        baseInterface = re.sub(r'\/[0-8]','',positional)
-        interfaceSpeed = interfaces[interface]['modes'][-1]['link']['speed']
-        speedList.append({'interface': interface,'baseInterface': int(baseInterface), 'interfaceSpeed': interfaceSpeed})
-    newlist = sorted(speedList, key=itemgetter('baseInterface'))
-    pprint(newlist)
+  for switch in switches:
+    try:
+        url = "https://%s:%s@%s/command-api" % (user, passwd, switch)
+        ss = Server(url)
+        #CONNECT TO DEVICE
+        hostname = ss.runCmds( 1, ['enable', 'show hostname' ])[1]['hostname']
+        interfaces = ss.runCmds( 1, ['enable', 'show interfaces hardware default' ])[1]['interfaces']
+        device_type = ss.runCmds( 1, ['enable', 'show inventory'])[1]['systemInformation']['name']
+        speedList = []
+        speedList.append({'deviceType': device_type, 'baseInterface': 0})
+        for interface in interfaces:
+            #print interfaces[interface]
+            positional = interface.replace('Ethernet','')
+            baseInterface = re.sub(r'\/[0-8]','',positional)
+            transceiverType = interfaces[interface]['transceiverType']
+            interfaceSpeed = interfaces[interface]['modes'][-1]['link']['speed']
+            speedList.append({'interface': interface,'baseInterface': int(baseInterface), 'interfaceSpeed': interfaceSpeed, 'transceiverType': transceiverType})
+        newlist = sorted(speedList, key=itemgetter('baseInterface'))
+        print newlist
+    except:
+        print 'failure on '+switch
 
 
 if __name__ == "__main__":
