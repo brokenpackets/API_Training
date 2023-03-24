0a3feffb971fed41ae184ba8657a965d95eb7b60
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2023-03-24 13:44:52 -0500

    Create bugAlert-HWLifecycle.py

---
+++ bugAlert-HWLifecycle.py
@@ -0,0 +1,20 @@
+import json
+import requests
+import base64
+from pprint import pformat
+
+token = '<your arista.com token from user profile>'
+url = 'https://www.arista.com/custom_data/bug-alert/alertBaseDownloadApi.php'
+
+encodedtoken_bytes = token.encode("ascii")
+encodedtoken = base64.b64encode(encodedtoken_bytes)
+jsonpost = {'token_auth':encodedtoken.decode("ascii"),'file_version':'2'}
+
+result = requests.post(url, data=json.dumps(jsonpost))
+output = result.json()
+for item in output['hardwareLifecycles']:
+  print('Model: '+item['modelName'])
+  print('End of Sale: '+item['endOfSale'])
+  print('End of Life: '+item['endOfLife'])
+  print('End of TAC Support: '+item['endOfTACSupport'])
+  print('')
