38a5ce7ca19c48908785fc3626c8aa4d73a90d23
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2025-04-03 15:46:15 -0700

    Update CVP_Token_RunCmds.py

--- CVP_Token_RunCmds.py
+++ CVP_Token_RunCmds.py
@@ -3,22 +3,24 @@ import requests
 import json
 
 ###### User Variables
-cmds = ['show ip bgp summary vrf <vrf>']
-server1 = 'https://192.168.255.50'
-token = 'your_token_goes_here'
-
+cmds = ['show ip bgp'] # command(s) to run, comma separated, as strings within a list.
+server1 = 'https://www.cv-prod-na-northeast1-b.arista.io' # change this to your CV tenant (www. must be included)
+token = 'your-token-here' # generate a service account and token on your CV tenant.
+devices = ['192.168.13.9']
 ###### Rest of script.
 connect_timeout = 10
-headers = {"Accept": "application/json",
+headers2 = {"Accept": "application/json",
            "Content-Type": "application/json",
-           "Authorization": "Bearer"+token}
+           "Authorization": "Bearer "+token}
 requests.packages.urllib3.disable_warnings()
 session = requests.Session()
+session.headers.update(headers2)
 
 def run_commands(url_prefix,cmds,deviceId):
-    data = json.dumps({"cmds":cmds,"host":deviceId})
-    response = session.post(url_prefix+'/cvpservice/di/internal/runcmds', data=data)
-    return response.json()
-
-output = run_commands(server1,cmds,'192.168.255.253')
+  data = json.dumps({"cmds":cmds,"host":deviceId})
+  response = session.post(url_prefix+'/cvpservice/di/internal/runcmds', data=data)
+  return response.json()
+output = []
+for device in devices:
+  output.append({device:run_commands(server1,cmds,device)})
 print(output)
